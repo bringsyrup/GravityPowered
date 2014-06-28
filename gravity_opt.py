6c2fc31a2ea3690f7c80b3a44e880b1594e50cb0
Ruby Spring <rubymspring@gmail.com>
2014-06-28 02:00:49 -0400

    shorter and cleaner, same funcionality

--- gravity_opt.py
+++ gravity_opt.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python
 
 '''
-** this version can be (optionally) geared up n times such that the gear 
-ratio is (R/r)^(n-1). Else it assumes n = 5.
+** use optional positional argument "n" for a gear ratio of (R/r)^n. Else n = 5.
 '''
 import numpy as np
 from matplotlib.pyplot import * 
@@ -14,27 +13,29 @@ parser.add_argument("P", type=float, help="power load, W")
 parser.add_argument("t", type=float, help="desired run-time, minutes")
 parser.add_argument("n", type=int, nargs='?', help="integer number of gear-ups, integer")
 parser.add_argument("-v", "--verbose", action="store_true")
-parser.add_argument("-s", "--specs", action="store_true", help="using this option prints important \
-	information about the given specs on the alternator \
-	used to calcuate the outputs of this script")
+parser.add_argument("-s", "--specs", action="store_true", help="print characterization graphs for generator")
 args = parser.parse_args()
 
-def alternator_data(P): #the data in this function is a placeholder!
-	if P == args.P:
-		P_mech = P / efficiency
-		P_mech_data = np.linspace(0, 100, 11)	
-		omega_data = [0., 30., 60., 80., 95., 100., 105., 115., 135., 160., 200.]
-		coefficients = np.polyfit(P_mech_data, omega_data, 4)
-		pack = [P_mech, coefficients]
-		return pack
-	elif P == None:
-		P_mech_data = np.linspace(0, 100, 11)	
-		omega_data = [0., 30., 60., 80., 95., 100., 105., 115., 135., 160., 200.]
-		coefficients = np.polyfit(P_mech_data, omega_data, 4)
-		return coefficients
+def alternator_data(): #the data in this function is a placeholder!
+	P_mech_data = np.linspace(0, 100, 11)	
+	omega_data = [0., 30., 60., 80., 95., 100., 105., 115., 135., 160., 200.]
+	coefficients = np.polyfit(P_mech_data, omega_data, 4)
+	return coefficients
+
+def get_output(n):
+	m_weight = torque_gen * pow(R, n-1) / (g * pow(r, n))
+	v_weight = omega * pow(r, n) / pow(R, n-1)
+	dist = v_weight * t_sec
+	pack_ans = [m_weight, v_weight, dist]
+	return pack_ans
+
+def sigfigs(x):
+    format = "%.2e"
+    as_string = format % x
+    return as_string
 
 if args.specs:
-	coefficients = alternator_data(None)
+	coefficients = alternator_data()
 	omega_poly = np.poly1d(coefficients)
 	P_mechs = np.arange(0, 100, 1)
 	omegas = omega_poly(P_mechs)
@@ -53,7 +54,6 @@ if args.specs:
 	ylabel('Rotational frequency of alternator')
 	suptitle('Alternator characterization', fontsize=20)
 	show()
-	
 else:
 	g = 9.8 #gravitational constant, m/s^2
 	r = .0635 #small gear radius, m
@@ -63,37 +63,24 @@ else:
 	efficiency = -0.0002 * pow(args.P - 50, 2) + 0.5 #this equation is a placeholder
 
 	if efficiency <= 0:
-		print "not a valid power input for this generator. Try option -s for more info."
+		print "not a valid power load for this generator. Try option --specs for more info."
 	else:
-		unpack = alternator_data(args.P)
-		coefficients = unpack[1]
-		P_mech = unpack[0]
+		P_mech = args.P / efficiency
+		coefficients = alternator_data()
 		omega = 0.
 		for coefficient in range(len(coefficients)):
 			omega += coefficients[coefficient] * pow(P_mech, 4 - coefficient)
-
 		torque_gen = P_mech / omega
 
 		if args.n:
-			n = args.n
-			m_weight = torque_gen * pow(R, n-1) / (g * pow(r, n))
-			v_weight = omega * pow(r, n) / pow(R, n-1)
-			dist = v_weight * t_sec
+			unpack_ans = get_output(args.n)
 		else:
-			n = 5
-			m_weight = torque_gen * pow(R, n-1) / (g * pow(r, n))
-			v_weight = omega * pow(r, n) / pow(R, n-1)
-			dist = v_weight * t_sec
-
-		def sigfigs(x):
-		    format = "%.2e"
-		    as_string = format % x
-		    return as_string
+			unpack_ans = get_output(5)
 
 		#convert answers to sci notation with 3 sig figs
-		Dist = sigfigs(dist)
-		V_weight = sigfigs(v_weight)
-		M_weight = sigfigs(m_weight)
+		M_weight = sigfigs(unpack_ans[0])
+		V_weight = sigfigs(unpack_ans[1])
+		Dist = sigfigs(unpack_ans[2])
 
 		if args.verbose:
 			print "%s m required at %s m/s using a %s kg mass" % (Dist, V_weight, M_weight)
