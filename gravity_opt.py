88f8544f20b42698ef17f725ae6b84a3c7ebce18
Ruby Spring <rubymspring@gmail.com>
2014-07-01 15:24:26 -0400

    added README, gravGen.pdf, and changed gravity_opt.py

--- gravity_opt.py
+++ gravity_opt.py
@@ -8,28 +8,11 @@ import math as m
 import numpy as np
 from matplotlib.pyplot import *
 import argparse as ap
+import constants as c
 
-parser = ap.ArgumentParser(description="Gravity-powered generator optimization")
-parser.add_argument("max_mass", type=int, help="max mass allowed in optimization")
-parser.add_argument("time", type=float, help="minimum desired run-time")
-parser.add_argument("-s", "--specs", action="store_true", help="prints characterization data for generator")
-args = parser.parse_args()
-
-#defined constants
-r = .0635 #small gear radius, m
-R = .3429 #large gear radius, m
-dist = 1.5 #max practical distance allowed
-g = 9.8 #gravitational constant, m/s^2
-P_mech_range = 400 
-'''P_mech_range is currently used to generate the mechanical power range, but when /
-data exists get rid of P_mech_range and replace P_mech_data with a data array. /
-Similarly, all other current data arrays are stand-ins until real data is aquired. /
-If more than one data set is available, I should figure out how to import or argparse arrays /
-from a script containing the data sets.
-'''
 def generate_curves(max_P):
     #generate P_load vs P_mech curve from P_mech and P_load data
-    P_mech_data = np.linspace(1, P_mech_range, 11)
+    P_mech_data = np.linspace(1, c.P_mech_range, 11)
     P_load_data = [0., 10., 30., 60., 100., 140., 175., 190., 195., 198., 200.]
     coeffs_load = np.polyfit(P_mech_data, P_load_data, 4)
     P_load_poly = np.poly1d(coeffs_load)
@@ -51,39 +34,61 @@ def generate_curves(max_P):
     else:
         return P_mechs, P_load, omega, efficiency 
 
-def get_gears(radius, Radius, mass, power, omega, gravity):
-    base = radius / Radius
-    arg = power / (Radius * omega * mass * gravity)
+def get_gears(mass, power, omega):
+    base = c.r / c.R
+    arg = power / (c.R * omega * mass * c.g)
     gears = m.log(arg, base)
     return gears
 
-#if specs option on, show full generator characterization curves
-if args.specs:
-    axis = 14 #set axis fontsize
-    line = 3.
-    P_mechs, P_load, omega, eff = generate_curves(P_mech_range)
-    subplot(311)
-    plot(P_mechs, omega, linewidth=line)
-    xlabel("mechanical power input, W", fontsize=axis)
-    ylabel("rotational velocity, rad/s", fontsize=axis)
-    subplot(312)
-    plot(P_mechs, P_load, linewidth=line)
-    xlabel("mechanical power input, W", fontsize=axis)
-    ylabel("electrical power output, W", fontsize=axis)
-    subplot(313)
-    plot(P_load, eff, linewidth=line)
-    xlabel("electrical power output, W", fontsize=axis)
-    ylabel("power efficiency", fontsize=axis)
-    suptitle("full characterization curves from data", fontsize=20)
-    show()
-#else, get optimal values for load, mass, and number of gear-ups
-else:
-    P_mech_max = args.max_mass * g * dist / args.time
-    P_mechs, P_load, omega_poly, coeffs_load, coeffs_eff, eff = generate_curves(P_mech_max)
-    eff_opt = max(eff)
-    P_load_opt = P_load[np.where(eff==eff_opt)]
-    P_mech_opt = P_mechs[np.where(P_load==P_load_opt)]
-    omega_opt = omega_poly(P_mech_opt)
-    mass_opt = P_mech_opt * args.time / (g * dist)
-    gears_opt = get_gears(r, R, mass_opt, P_mech_opt, omega_opt, g)
-    print " %d Watt load \n %d kg mass \n %d gear-ups" % (P_load_opt, mass_opt, gears_opt)
+def sigfigs(as_float):
+    if type(as_float)=='numpy.ndarray':
+        as_float = float(as_float)
+    format = "%.2e"
+    as_string = format % as_float
+    return as_string
+
+if __name__=='__main__':
+
+    parser = ap.ArgumentParser(description="Gravity-powered generator optimization")
+    parser.add_argument("max_mass", type=int, help="max mass allowed in optimization")
+    parser.add_argument("time", type=float, help="minimum desired run-time")
+    parser.add_argument("-s", "--specs", action="store_true", help="prints characterization data for generator")
+    parser.add_argument("-v", "--verbose", action="store_true", help="print answers in sci notation with 3 significant figures instead of as integers")
+    args = parser.parse_args()
+
+        #if specs option on, show full generator characterization curves
+    if args.specs:
+        axis = 14 #set axis fontsize
+        line = 3.
+        P_mechs, P_load, omega, eff = generate_curves(P_mech_range)
+        subplot(311)
+        plot(P_mechs, omega, linewidth=line)
+        xlabel("mechanical power input, W", fontsize=axis)
+        ylabel("rotational velocity, rad/s", fontsize=axis)
+        subplot(312)
+        plot(P_mechs, P_load, linewidth=line)
+        xlabel("mechanical power input, W", fontsize=axis)
+        ylabel("electrical power output, W", fontsize=axis)
+        subplot(313)
+        plot(P_load, eff, linewidth=line)
+        xlabel("electrical power output, W", fontsize=axis)
+        ylabel("power efficiency", fontsize=axis)
+        suptitle("full characterization curves from data", fontsize=20)
+        show()
+    #else, get optimal values for load, mass, and number of gear-ups
+    else:
+        P_mech_max = args.max_mass * c.g * c.dist / args.time
+        P_mechs, P_load, omega_poly, coeffs_load, coeffs_eff, eff = generate_curves(P_mech_max)
+        eff_opt = max(eff)
+        P_load_opt = P_load[np.where(eff==eff_opt)]
+        P_mech_opt = P_mechs[np.where(P_load==P_load_opt)]
+        omega_opt = omega_poly(P_mech_opt)
+        mass_opt = P_mech_opt * args.time / (c.g * c.dist)
+        gears_opt = get_gears(mass_opt, P_mech_opt, omega_opt)
+        if args.verbose:
+            P_load_opt = sigfigs(P_load_opt)
+            mass_opt = sigfigs(mass_opt)
+            gears_opt = sigfigs(gears_opt)
+            print " %s Watt load \n %s kg mass \n %s gear-ups" % (P_load_opt, mass_opt, gears_opt)
+        else:
+            print " %d Watt load \n %d kg mass \n %d gear-ups" % (P_load_opt, mass_opt, gears_opt)
